a4587a3dd31eecb226ec18e4ed1b8d830791e992
CGmaybe10 <409958786@qq.com>
2016-09-13 14:31:59 +0800

    调整目录

--- app/src/main/java/com/freegank/activity/MainActivity.java
+++ app/src/main/java/com/freegank/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.freegank;
+package com.freegank.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.freegank.R;
 
 public class MainActivity extends AppCompatActivity {
 
