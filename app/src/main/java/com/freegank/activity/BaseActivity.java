bd23b17df982a238b462d77b3f8da2048e07f438
CGmaybe10 <409958786@qq.com>
2016-09-27 19:07:08 +0800

    添加隐藏toolbar的功能

--- app/src/main/java/com/freegank/activity/BaseActivity.java
+++ app/src/main/java/com/freegank/activity/BaseActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
 
 import com.freegank.R;
 
@@ -14,6 +15,10 @@ import com.freegank.R;
  * 这是适配状态栏透明
  */
 public abstract class BaseActivity extends AppCompatActivity {
+    private final String TAG = "moubiao";
+
+    protected Toolbar mToolbar;
+    protected boolean isToolBarHiding = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +36,18 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
     private void initToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
         }
+    }
+
+    protected void showOrHideToolbar() {
+        mToolbar.animate()
+                .translationY(isToolBarHiding ? 0 : -mToolbar.getHeight())
+                .setInterpolator(new DecelerateInterpolator(2))
+                .start();
+        isToolBarHiding = !isToolBarHiding;
     }
 
     public abstract int getLayoutResId();
--- app/src/main/java/com/freegank/activity/MeiZhiActivity.java
+++ app/src/main/java/com/freegank/activity/MeiZhiActivity.java
@@ -2,6 +2,8 @@ package com.freegank.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.freegank.R;
@@ -39,11 +41,31 @@ public class MeiZhiActivity extends BaseActivity {
     }
 
     private void initView() {
+        hideToolbar();
         mMeiZhiIMG = (ImageView) findViewById(R.id.mei_zhi_img);
         Picasso.with(this)
                 .load(IMG_URL)
                 .placeholder(R.drawable.default_ic)
                 .error(R.drawable.error_ic)
                 .into(mMeiZhiIMG);
+
+        mMeiZhiIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOrHideToolbar();
+            }
+        });
+    }
+
+    /**
+     * 隐藏toolbar
+     */
+    private void hideToolbar() {
+        TypedValue typedValue = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
+            int height = TypedValue.complexToDimensionPixelSize(typedValue.data, getResources().getDisplayMetrics());
+            mToolbar.setTranslationY(-height);
+            isToolBarHiding = true;
+        }
     }
 }
