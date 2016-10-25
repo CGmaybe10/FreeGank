ccb7b6d439cb9928613f2d1f83513014f2eebdba
CGmaybe10 <409958786@qq.com>
2016-10-25 17:34:17 +0800

    完成每日详情界面的图片延伸到状态栏功能

--- app/src/main/java/com/freegank/activity/DailyDetailActivity.java
+++ app/src/main/java/com/freegank/activity/DailyDetailActivity.java
@@ -1,6 +1,7 @@
 package com.freegank.activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -56,6 +57,16 @@ public class DailyDetailActivity extends BaseActivity implements OnItemClickList
 
         initData();
         initView();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+        }
     }
 
     private void initData() {
